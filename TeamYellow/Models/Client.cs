613e241266f6d27370d3ed8883e41528549b9db0
Jimmy <jzhang522@my.bcit.ca>
2026-02-03 04:51:31 -0800

    Create the Client Model

---
+++ TeamYellow/Models/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamYellow.Models;
+
+public class Client
+{
+    public int ClientId { get; set; }
+
+    [Required, MaxLength(50)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required, MaxLength(50)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required, MaxLength(255)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, MaxLength(20)]
+    public string Phone { get; set; } = string.Empty;
+
+    [Required, MaxLength(20)]
+    public string Status { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int CounsellorId { get; set; }
+
+    public virtual Counsellor Counsellor { get; set; } = null!;
+}
