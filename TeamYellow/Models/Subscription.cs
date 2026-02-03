45901a1ff1b1a41ed368194331a001bc81736140
thinh <dqthinh16@gmail.com>
2026-02-03 11:39:44 -0800

    update columns based on ERD

--- TeamYellow/Models/Subscription.cs
+++ TeamYellow/Models/Subscription.cs
@@ -1,16 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 namespace TeamYellow.Models;
 
 public class Subscription
 {
-    public int PlanFeaturedId;
-
-    [MaxLength(120)]
-    public required string featureName;
-
-    [MaxLength(300)]
-    public required string featureDescription;
+    [Key]
+    public int SubscriptionId { get; set; }
+    public required bool IsActive { get; set; }
+    public required DateTime CycleStart { get; set; }
+    public required DateTime CycleEnd { get; set; }
+    public required DateTime CreateAt { get; set; }
+    public DateTime? UpdateAt { get; set; }
 
 }
