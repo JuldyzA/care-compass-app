cb5cf63c595c499eed507ca9e9a2ca49b3476714
Mahima Sharma - BCIT <msharma163@my.bcit.ca>
2026-02-03 11:52:04 -0800

    chore: update namespace braces to file-scoped format

--- TeamYellow/Models/Discount.cs
+++ TeamYellow/Models/Discount.cs
@@ -1,41 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TeamYellow.Models
+namespace TeamYellow.Models;
+
+public enum DiscountType
 {
-    public enum DiscountType
-    {
-        Percent = 0,   // %
-        Amount = 1     // $
-    }
+    Percent = 0,   // %
+    Amount = 1     // $
+}
 
-    [Table("Discount")]
-    public class Discount
-    {
-        [Key]
-        [Column("pkDiscountId")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int DiscountId { get; set; }
+[Table("Discount")]
+public class Discount
+{
+    [Key]
+    [Column("pkDiscountId")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int DiscountId { get; set; }
 
-        [Required]
-        [Column("discountCode")]
-        [MaxLength(40)]
-        public string DiscountCode { get; set; } = null!;
+    [Required]
+    [Column("discountCode")]
+    [MaxLength(40)]
+    public string DiscountCode { get; set; } = null!;
 
-        [Required]
-        [Column("discountType")]
-        public DiscountType DiscountType { get; set; } = DiscountType.Percent;
+    [Required]
+    [Column("discountType")]
+    public DiscountType DiscountType { get; set; } = DiscountType.Percent;
 
-        [Column("value")]
-        public decimal Value { get; set; }
+    [Column("value")]
+    public decimal Value { get; set; }
 
-        [Column("startDateTime")]
-        public DateTime StartDateTime { get; set; }
+    [Column("startDateTime")]
+    public DateTime StartDateTime { get; set; }
 
-        [Column("endDateTime")]
-        public DateTime EndDateTime { get; set; }
+    [Column("endDateTime")]
+    public DateTime EndDateTime { get; set; }
 
-        [Column("createdAt")]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    }
+    [Column("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
