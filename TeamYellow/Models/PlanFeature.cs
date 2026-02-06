4d3fed16732fa2c5bd8d1801ae06022855cf3bd7
Jin Ming Zhang <jzhang522@my.bcit.ca>
2026-02-06 11:15:31 -0800

    Refactor: PlanFeature class structure and formatting

--- TeamYellow/Models/PlanFeature.cs
+++ TeamYellow/Models/PlanFeature.cs
@@ -1,31 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TeamYellow.Models
-{
-    [Table("PlanFeature")]
-    public class PlanFeature
-    {   
-        [Key]
-        [Column("pkPlanFeatureId")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int PlanFeatureId { get; set; }
+namespace TeamYellow.Models;
 
-        [Column("featureName")]
-        [Required, MaxLength(120)]
-        public string FeatureName { get; set; } = String.Empty;
+[Table("PlanFeature")]
+public class PlanFeature
+{   
+    [Key]
+    [Column("pkPlanFeatureId")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PlanFeatureId { get; set; }
 
+    [Column("featureName")]
+    [Required, MaxLength(120)]
+    public string FeatureName { get; set; } = String.Empty;
 
-        [Column("featureDescription")]
-        [Required, MaxLength(300)]
-        public string FeatureDescription { get; set; } = String.Empty;
-        
-        [Column("sortOrder")]
-        public int sortOrder { get; set; }
-
-       
-        [Column("fkPlanId")]
-        public int PlanId { get; set; }
-        public Plan Plan { get; set; } = null!;
-    }
+    [Column("featureDescription")]
+    [Required, MaxLength(300)]
+    public string FeatureDescription { get; set; } = String.Empty;
+    
+    [Column("sortOrder")]
+    public int sortOrder { get; set; }
+   
+    [Column("fkPlanId")]
+    public int PlanId { get; set; }
+    
+    public virtual Plan Plan { get; set; } = null!;
 }
