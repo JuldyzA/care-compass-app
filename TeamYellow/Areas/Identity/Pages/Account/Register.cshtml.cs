7134005417d6511e51bc2239675eff021208b33c
susielarson <susielarson93@gmail.com>
2026-02-12 15:44:20 -0800

    feature: add email service Brevo
    On register Brevo will send confirmation email

--- TeamYellow/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TeamYellow/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,6 +18,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using TeamYellow.Models;
+using TeamYellow.Services;
 
 namespace TeamYellow.Areas.Identity.Pages.Account
 {
@@ -28,21 +30,21 @@ namespace TeamYellow.Areas.Identity.Pages.Account
         private readonly IUserStore<IdentityUser> _userStore;
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+        private readonly IEmailService _emailService;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailService emailService)
         {
             _userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
+            _emailService = emailService;
         }
 
         /// <summary>
@@ -131,8 +133,13 @@ namespace TeamYellow.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    ComposeEmailModel payload = new ComposeEmailModel
+                    {
+                        Email = Input.Email,
+                        Subject = "Confirm your email",
+                        Body = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>."
+                    };
+                    await _emailService.SendEmailAsync(payload);
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
