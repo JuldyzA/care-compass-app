8438c95becf643d34c46a97e8086d52dddaddf47
susielarson <susielarson93@gmail.com>
2026-02-14 18:05:45 -0800

    feature: add first and last name to register page

--- TeamYellow/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TeamYellow/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -103,6 +103,16 @@ namespace TeamYellow.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
         }
 
 
