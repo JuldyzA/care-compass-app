39a27c7b26728e54268be9a4f8b09a4497597b55
Mahima Sharma - BCIT <msharma163@my.bcit.ca>
2026-03-03 21:37:08 -0800

    chore: UI changes to add sidebar and display index pages according to the role

---
+++ TeamYellow/Views/Shared/_DashboardLayout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamYellow.Views.Shared
+{
+    public class _DashboardLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TeamYellow/Views/Shared/_Sidebar.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TeamYellow.Views.Shared
+{
+    public class _SidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
