6b26293f5071a13a3c0a568f791618ed1041a3dd
Juldyz Akhmetova <84645560+JuldyzA@users.noreply.github.com>
2026-03-18 12:43:16 -0700

    Added images, aligned layout, havigation and footer layout fixed, added responsiveness, added aditional colors, icons, videos

--- TeamYellow/Controllers/HomeController.cs
+++ TeamYellow/Controllers/HomeController.cs
@@ -41,6 +41,11 @@ namespace TeamYellow.Controllers
             return View();
         }
 
+        public IActionResult Solution()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
